4ef4ab6396cb42a7ff5e833759e8f346ec78fdd8
i-m-vivek <vivek.mittal051@gmail.com>
2021-11-24 22:40:59 +0530

    added base and arm policy paths

--- hrl4in/scripts/train_hrl4in_base_only_arm_only_fixed.py
+++ hrl4in/scripts/train_hrl4in_base_only_arm_only_fixed.py
@@ -779,8 +779,8 @@ def main():
     )
 
     # load pretrained LL policy
-    base_ckpt_path = ""
-    arm_ckpt_path = ""
+    base_ckpt_path = "/home/guest/vivek_ws/iGibson/HRL4IN/hrl4in/scripts/ckpt/ppo_tiago_base_reaching_without_orn/ckpt/ckpt.2200.pth"
+    arm_ckpt_path = "/home/guest/vivek_ws/iGibson/HRL4IN/hrl4in/scripts/ckpt/ppo_tiago_arm_reaching/ckpt.2700.pth"
     
     ckpt = torch.load(base_ckpt_path, map_location=device)
     base_agent.load_state_dict(ckpt["state_dict"])
